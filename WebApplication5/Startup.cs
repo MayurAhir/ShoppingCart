19df7c5770d915cef5ebc7bcec61213195130d82
MayurAhir <mayurahir4045@gmail.com>
2017-07-13 15:42:41 -0400

    Add project files.

---
+++ WebApplication5/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebApplication5.Startup))]
+namespace WebApplication5
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
